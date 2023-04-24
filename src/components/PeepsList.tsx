73a6cbbd92f1caaea5dcf6f411d96ef011a15bb5
Christian Moen <christian@innomar.no>
2023-04-24 17:03:04 +0200

    changed it to be more mobile friendly

--- src/components/PeepsList.tsx
+++ src/components/PeepsList.tsx
@@ -1,51 +1,79 @@
-import dayjs from "dayjs";
-import { api } from "~/utils/api";
-import { AiOutlineHeart, AiFillHeart, AiOutlineDelete } from "react-icons/ai";
-
-import { useState } from "react";
+/* eslint-disable @next/next/no-img-element */
+import dayjs, { Dayjs } from "dayjs";
 import { useSession } from "next-auth/react";
+import { AiFillHeart, AiOutlineDelete, AiOutlineHeart } from "react-icons/ai";
+import { api } from "~/utils/api";
+import durationPlugin from "dayjs/plugin/duration";
+dayjs.extend(durationPlugin);
 
 // peep list is a component that displays a list of peeps/tweets
+
+function formatDateSince(duration: durationPlugin.Duration) {
+    const hours = duration.asHours();
+    const minutes = duration.asMinutes();
+
+    if (hours > 1) {
+        return `${hours.toFixed(0)}h`;
+    }
+
+    return `${minutes.toFixed(0)}m`;
+}
+
 export function PeepsList() {
     const { data } = useSession();
     const { isLoading, data: peepsData } = api.peep.getAll.useQuery();
 
-    if (isLoading) {
-        return <div>Loading...</div>;
-    }
-
     return (
-        <div className="space-y-4">
+        <div className="w-full flex flex-col gap-4  ">
+            {isLoading && (
+                <div className="flex w-full items-center justify-center">
+                    <div className="animate-spin w-5 h-5 border-t-2 border-b-2 border-white rounded-full text-center"></div>
+                </div>
+            )}
             {peepsData?.map((peep) => {
                 const isAuthor = peep.author.id == data?.user?.id;
+
+                // if under a day ago, display like 11m // 1h // 1d
+                const createdAt = dayjs(peep.createdAt);
+                const now = dayjs();
+                const duration = dayjs.duration(now.diff(createdAt));
+                const agoTextTwitterLike = formatDateSince(duration);
 
                 return (
                     <div
                         key={peep.id}
-                        className="flex flex-row space-x-4 rounded-md p-4"
+                        className="flex flex-col md:flex-row space-y-4 md:space-y-0 md:space-x-4 w-full first-letter:py-4 border-b-2 border-slate-700 pb-2"
                     >
-                        <div className="flex flex-col">
-                            <div className="flex flex-row space-x-2 items-center">
-                                <div className="text-white text-lg font-bold">
-                                    {peep.author.name}
-                                </div>
-                                <div className="text-gray-500">
-                                    @{peep.author.name}
-                                </div>
-                                <div className="text-gray-500">·</div>
-                                <div className="text-gray-500">
-                                    {dayjs(peep.createdAt).format("MMM D")}
-                                </div>
+                        <div className="flex flex-row items-start space-x-4">
+                            <div id="avatar" className="w-12 h-12 rounded-full">
+                                <img
+                                    src={peep.author.image ?? ""}
+                                    alt="avatar"
+                                />
                             </div>
-                            <div className="text-white mt-1">
-                                {peep.content}
+                            <div className="flex flex-col gap-2">
+                                <div className="flex flex-row space-x-2 items-center">
+                                    <div className="text-white text-base font-semibold">
+                                        {peep.author.name}
+                                    </div>
+                                    <div className="text-gray-500">
+                                        @{peep.author.name}
+                                    </div>
+                                    <div className="text-gray-500">·</div>
+                                    <div className="text-gray-500">
+                                        {agoTextTwitterLike}
+                                    </div>
+                                </div>
+                                <div className="text-white mt-1 text-base ">
+                                    {peep.content}
+                                </div>
+                                <PeepActions
+                                    peepId={peep.id}
+                                    isAuthor={isAuthor}
+                                    likeCount={peep.likeCount}
+                                    likedByUser={peep.hasLiked}
+                                />
                             </div>
-                            <PeepActions
-                                peepId={peep.id}
-                                isAuthor={isAuthor}
-                                likeCount={peep.likeCount}
-                                likedByUser={peep.hasLiked}
-                            />
                         </div>
                     </div>
                 );
@@ -88,7 +116,7 @@ export function PeepActions({
     };
 
     return (
-        <div className="flex flex-row space-x-4">
+        <div className="flex flex-row space-x-8">
             <button
                 className="flex flex-row items-center space-x-2 focus:outline-none hover:text-red-500"
                 onClick={handleLikeClick}
@@ -99,9 +127,15 @@ export function PeepActions({
                 ) : (
                     <>
                         {likedByUser ? (
-                            <AiFillHeart className="text-pink-500 hover:text-pink-700" />
+                            <AiFillHeart
+                                size={20}
+                                className="text-pink-500 hover:text-pink-700"
+                            />
                         ) : (
-                            <AiOutlineHeart className="text-white hover:text-pink-700" />
+                            <AiOutlineHeart
+                                size={20}
+                                className="text-white hover:text-pink-700"
+                            />
                         )}
                         <div className="text-gray-500">{likeCount ?? 0}</div>
                     </>
@@ -117,7 +151,7 @@ export function PeepActions({
                         <div className="animate-spin w-5 h-5 border-t-2 border-b-2 border-white rounded-full"></div>
                     ) : (
                         <>
-                            <AiOutlineDelete className="fill-white" />
+                            <AiOutlineDelete size={20} className="fill-white" />
                             <div className="text-gray-500">Delete</div>
                         </>
                     )}
--- src/components/PostPeepForm.tsx
+++ src/components/PostPeepForm.tsx
@@ -13,7 +13,7 @@ export const PostPeepForm = () => {
     return (
         <form
             ref={refReset}
-            className="w-full flex-row flex"
+            className="w-full flex flex-col md:flex-row"
             onSubmit={async (e) => {
                 e.preventDefault();
                 const formData = new FormData(e.currentTarget);
@@ -24,18 +24,25 @@ export const PostPeepForm = () => {
                 }
             }}
         >
-            <div className="flex flex-grow">
+            <div className="flex flex-grow mb-4 md:mb-0">
                 <input
                     name="content"
-                    className="w-full bg-white text-black rounded-l-full border-gray-500 p-3 text-xl"
+                    disabled={peepMutation.isLoading}
+                    className="w-full bg-white text-black rounded-l-lg md:rounded-none md:rounded-l-lg border-gray-500 p-3 text-xl"
                 />
             </div>
             <button
-                className="bg-blue-500 w-2/12 h-15 hover:bg-blue-700 text-white font-bold rounded-r-full"
+                className="bg-blue-500 w-4/12 h-15 hover:bg-blue-700 text-white font-bold rounded-r-lg md:rounded-none md:rounded-r-lg"
                 type="submit"
                 disabled={peepMutation.isLoading}
             >
-                {peepMutation.isLoading ? "Loading..." : "Peep"}
+                {peepMutation.isLoading ? (
+                    <div className="flex w-full items-center justify-center">
+                        <div className="animate-spin w-5 h-5 border-t-2 border-b-2 border-white rounded-full text-center"></div>
+                    </div>
+                ) : (
+                    "Peep"
+                )}
             </button>
         </form>
     );
